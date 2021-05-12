beb3e05976e5011dfe09c18cccf7ad1104123cd8
Jerome Trombetta <jrmtbta@gmail.com>
2021-05-12 17:39:25 +0200

    new seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'csv'
 
-# City.destroy_all
-
+City.destroy_all
 # User.destroy_all
 
 # rennes = City.create!(
@@ -54,14 +53,18 @@ require 'csv'
 csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
 filepath    = 'db/fixtures/population.csv'
 
+@geocode_population = []
+
 # Pour chaque ligne du fichier
 CSV.foreach(filepath, csv_options) do |row|
   # je prends le geocode
   geocode = row['geocode']
+  @geocode_population << geocode
+
   department_code = [22, 29, 35, 56]
 
   # # je regarde s'il commence par 22, 29, 35, 56
-  next unless department_code.include?(geocode[0..1])
+  next unless department_code.include?(geocode[0..1].to_i)
 
   # Je retrouve la ville en DB avec le meme nom
   # Si il n'y en a pas, je crée une nouvelle ville avec ce nom
@@ -74,25 +77,96 @@ end
 
 
 
+p "Medical services seeds incoming"
+# Ouvrir le fichier service-medicaux.csv
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath    = 'db/fixtures/service-medicaux.csv'
+
+# Pour chaque ligne du fichier
+CSV.foreach(filepath, csv_options) do |row|
+  # je prends le geocode
+  geocode = row['geocode']
+  department_code = [22, 29, 35, 56]
+  # # je regarde s'il commence par 22, 29, 35, 56
+  next unless (department_code.include?(geocode[0..1].to_i) && @geocode_population.include?(geocode))
+
+  # Je retrouve la ville en DB avec le meme nom
+  # Si il n'y en a pas, je crée une nouvelle ville avec ce nom
+  city = City.find_or_initialize_by(geocode: geocode)
+  # je mets à jour ma ville en db avec la population du csv
+  doctor = false
+  city_doctor = row['doctor'].to_i
+  doctor = true if city_doctor > 0
+  city.update(name: row['city_name'], doctor: doctor)
+  # je sauve
+  city.save!
+end
 
 
-# selectionner toutes les villes dont le geocode commence par 22, 29, 35 et 56
-# Créer ces villes et leur attribuer la population (selectionner les bonnes colonnes)
+
+p "Population average age seeds incoming"
+# Ouvrir le fichier Age-moyen-population.csv
+csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
+filepath    = 'db/fixtures/Age-moyen-population.csv'
+
+# Pour chaque ligne du fichier
+CSV.foreach(filepath, csv_options) do |row|
+  # je prends le geocode
+  geocode = row['geocode']
+  department_code = [22, 29, 35, 56]
+
+  # je regarde s'il commence par 22, 29, 35, 56
+  next unless (department_code.include?(geocode[0..1].to_i) && @geocode_population.include?(geocode))
+
+  # Je retrouve la ville en DB avec le meme nom
+  # Si il n'y en a pas, je crée une nouvelle ville avec ce nom
+  city = City.find_or_initialize_by(geocode: geocode)
+  # je mets à jour ma ville en db avec la population du csv
+  city.update(age_average: row['average_age_population'].to_i)
+  # je sauve
+  city.save!
+end
 
 
+
+p "Commodities seeds incoming"
 # Ouvrir le fichier commerces.csv
-# Mettre à jour les villes existantes avec les data recherchées (selectionner les bonnes colonnes)
-# Ajouter les villes si elles n existent pas
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath    = 'db/fixtures/commerces.csv'
 
-# Ouvrir le fichier medical.csv
-# Mettre à jour les villes existantes avec les data recherchées (selectionner les bonnes colonnes)
-# Ajouter les villes si elles n existent pas
+# Pour chaque ligne du fichier
+CSV.foreach(filepath, csv_options) do |row|
+  # je prends le geocode
+  geocode = row['geocode']
+  department_code = [22, 29, 35, 56]
+  # # je regarde s'il commence par 22, 29, 35, 56
 
-# etc...
+  next unless (department_code.include?(geocode[0..1].to_i) && @geocode_population.include?(geocode))
+
+  # Je retrouve la ville en DB avec le meme nom
+  # Si il n'y en a pas, je crée une nouvelle ville avec ce nom
+  city = City.find_or_initialize_by(geocode: geocode)
+
+  # je compte le nombre de commerces
+  commodity_count = 0
+  supermarket = false
+
+  commodity_count = row['handiwork'].to_i + row['grocery'].to_i + row['bakery'].to_i\
+                    + row['butchery'].to_i + row['frozen'].to_i + row['fish_market'].to_i\
+                    + row['bookstore'].to_i + row['clothe'].to_i + row['appliance'].to_i\
+                    + row['shoestore'].to_i + row['it'].to_i + row['furniture'].to_i\
+                    + row['sport'].to_i + row['house'].to_i + row['hardware'].to_i\
+                    + row['cosmetic'].to_i + row['jewellery'].to_i + row['plant'].to_i\
+                    + row['optic'].to_i + row['medical_store'].to_i
 
 
+  supermarket_count = row['supermarket'].to_i + row['hypermarket'].to_i + row['store'].to_i
+  supermarket = true if supermarket_count > 0
 
-
-
+  # je mets à jour ma ville en db avec la population du csv
+  city.update(name: row['city_name'], commodity_count: commodity_count, supermarket: supermarket)
+  # je sauve
+  city.save!
+end
 
 

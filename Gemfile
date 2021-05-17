c1ec6d67930a44b11d21319d5d3f2df9a601c31b
Jerome Trombetta <jrmtbta@gmail.com>
2021-05-17 16:47:02 +0200

    add max population and max age criteria, except in rating calculation

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
 gem 'geocoder'
+gem 'has_scope'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
--- app/models/city.rb
+++ app/models/city.rb
@@ -3,4 +3,7 @@ class City < ApplicationRecord
   has_many :favorite_cities
   has_one_attached :photo
   validates :name, presence: true
+  scope :max_population, -> max_population { where("population < ?", max_population) }
+  scope :max_age_average, -> max_age { where("age_average < ?", max_age) }
+  scope :location, -> name, max_distance_km { near(name, max_distance_km) }
 end

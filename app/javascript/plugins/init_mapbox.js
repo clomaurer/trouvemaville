c07d0ed64835d5f9b523d2355ae7457570a2f36f
Clothilde Maurer <clothilde.maurer@gmail.com>
2021-05-17 17:15:38 +0200

    Added modal show and colored markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,11 +10,21 @@ const buildMap = (mapElement) => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
-    new mapboxgl.Marker()
+    const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.backgroundRepeat = 'no-repeat';
+      element.style.width = '25px';
+      element.style.height = '25px';
+      element.dataset.controller = 'city';
+      element.dataset.action = 'click->city#show';
+      element.dataset.cityTarget= 'marker';
+      element.dataset.id = marker.id;
+
+    new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
       .addTo(map);
   });
 };

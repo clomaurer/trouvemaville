713f87ae901fae4d87771b5b4618445efd6d880d
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-05-18 18:06:33 +0200

    add comparator button modal

--- app/controllers/compared_cities_controller.rb
+++ app/controllers/compared_cities_controller.rb
@@ -1,15 +1,20 @@
 class ComparedCitiesController < ApplicationController
   def create
     @city = City.find(params[:city_id])
-    @compared_city = ComparedCity.new
-    @compared_city.comparator = current_user.comparator
-    @compared_city.city = @city
-    @compared_city.save
-    # if @compared_city.save
-    #   render json: { success: true }
-    # else
-    #   render json: { success: false, errors: @compared_city.errors.messages }
-    # end
+
+    @compared_city = ComparedCity.find_by(comparator: current_user.comparator, city: @city)
+
+    if @compared_city
+      @compared_city.destroy
+      render json: { status: "destroyed" }
+    else
+      @compared_city = ComparedCity.new(comparator: current_user.comparator, city: @city)
+      if @compared_city.save
+        render json: { status: "created" }
+      else
+        render json: { status: "failed", errors: @compared_city.errors.messages }
+      end
+    end
   end
 
   def destroy
--- app/javascript/controllers/compared_controller.js
+++ app/javascript/controllers/compared_controller.js
@@ -18,7 +18,21 @@ export default class extends Controller {
         "Accept": "application/json",
         "Content-Type": "application/json"
       }
-    }).then(reponse => reponse.json()).then(data => console.log(data))
+    }).then(reponse => reponse.json()).then(data => {
+      if (data.status === "created") {
+        this.clipboardTarget.classList.add("fas")
+        this.clipboardTarget.classList.add("fa-clipboard-check")
+        this.clipboardTarget.classList.remove("fa-clipboard")
+        this.clipboardTarget.classList.remove("far")
+      } else if (data.status === "destroyed") {
+        this.clipboardTarget.classList.remove("fas")
+        this.clipboardTarget.classList.remove("fa-clipboard-check")
+        this.clipboardTarget.classList.add("fa-clipboard")
+        this.clipboardTarget.classList.add("far")
+      } else if (data.status === "failed") {
+        console.log(data.errors.base[0])
+      }
+    })
 
   }
 }

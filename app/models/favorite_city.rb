e58abc9aa6118a71f20861d69762092244c49084
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-05-18 12:38:03 +0200

    add cities and associated searches

--- app/models/favorite_city.rb
+++ app/models/favorite_city.rb
@@ -1,4 +1,5 @@
 class FavoriteCity < ApplicationRecord
   belongs_to :user
   belongs_to :city
+  belongs_to :saved_search
 end
---
+++ db/migrate/20210518082432_remove_user_id_from_saved_searches.rb
@@ -0,0 +1,5 @@
+class RemoveUserIdFromSavedSearches < ActiveRecord::Migration[6.0]
+  def change
+    remove_reference :saved_searches, :user, null: false, foreign_key: true
+  end
+end
